a79965734f0e17e5a37f12446a75d6d68a277077
ebadarshad <m.ebadarshad2003@gmail.com>
2026-01-19 11:22:44 +0500

    chore(backend): update api

--- app-tier/index.js
+++ app-tier/index.js
@@ -16,12 +16,12 @@ app.use(cors());
 // =======================================================
 
 //Health Checking
-app.get('/health',(req,res)=>{
+app.get('/api/health',(req,res)=>{
     res.json("This is the health check");
 });
 
 // ADD TRANSACTION
-app.post('/transaction', (req,res)=>{
+app.post('/api/transaction', (req,res)=>{
     var response = "";
     try{
         console.log(req.body);
@@ -35,7 +35,7 @@ app.post('/transaction', (req,res)=>{
 });
 
 // GET ALL TRANSACTIONS
-app.get('/transaction',(req,res)=>{
+app.get('/api/transaction',(req,res)=>{
     try{
         var transactionList = [];
        transactionService.getAllTransactions(function (results) {
@@ -53,7 +53,7 @@ app.get('/transaction',(req,res)=>{
 });
 
 //DELETE ALL TRANSACTIONS
-app.delete('/transaction',(req,res)=>{
+app.delete('/api/transaction',(req,res)=>{
     try{
         transactionService.deleteAllTransactions(function(result){
             res.statusCode = 200;
@@ -65,7 +65,7 @@ app.delete('/transaction',(req,res)=>{
 });
 
 //DELETE ONE TRANSACTION
-app.delete('/transaction/id', (req,res)=>{
+app.delete('/api/transaction/id', (req,res)=>{
     try{
         //probably need to do some kind of parameter checking
         transactionService.deleteTransactionById(req.body.id, function(result){
@@ -78,7 +78,7 @@ app.delete('/transaction/id', (req,res)=>{
 });
 
 //GET SINGLE TRANSACTION
-app.get('/transaction/id',(req,res)=>{
+app.get('/api/transaction/id',(req,res)=>{
     //also probably do some kind of parameter checking here
     try{
         transactionService.findTransactionById(req.body.id,function(result){
