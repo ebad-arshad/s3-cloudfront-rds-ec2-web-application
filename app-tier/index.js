52b8103ccca8cc04877320255e1272a41e880f42
ebadarshad <m.ebadarshad2003@gmail.com>
2026-01-19 12:55:38 +0500

    chore(backend): fix port 4000 to 5000

--- app-tier/index.js
+++ app-tier/index.js
@@ -6,7 +6,7 @@ const os = require('os');
 const fetch = require('node-fetch');
 
 const app = express();
-const port = 4000;
+const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
